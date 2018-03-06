5dc1f8b0ba512dbe8bed6b1bad0013120dbeb89e
yoshi <s.y.nakada@gmail.com>
2018-03-06 20:18:49 +0900

    Theo

--- Package.swift
+++ Package.swift
@@ -5,15 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "Neo4jData",
+    products: [
+        .library(name: "Neo4jDataCore", targets: ["Neo4jDataCore"]),
+        .executable(name: "Neo4jData", targets: ["Neo4jData"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Neo4j-Swift/Neo4j-Swift.git", from: "4.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Neo4jDataCore",
+            dependencies: ["Theo"]),
         .target(
             name: "Neo4jData",
-            dependencies: []),
+            dependencies: ["Neo4jDataCore"]),
+        .testTarget(
+            name: "Neo4jDataTests",
+            dependencies: ["Neo4jDataCore"]),
     ]
 )
